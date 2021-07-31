8b5cf5ecd1025e3b4c00a9cf6f734be5fd2a5d12
Tenotea <devtenotea@gmail.com>
2021-07-31 22:13:33 +0100

    update(hero): created image carousel

---
+++ assets/scripts/index.js
@@ -0,0 +1,30 @@
+const positionTrackers = document.querySelectorAll('.position-trackers .tracker')
+const carousel = document.querySelectorAll('.secondary-image')
+let lastImageIndex = 0
+
+const navigateCarousel = (index) => {
+  setPositionTrackerActiveState(positionTrackers, index)
+  if ( index === lastImageIndex ) return
+  let currentImage = carousel[index]
+  let lastImage = carousel[lastImageIndex]
+  currentImage.classList.add('primary-image')
+  lastImage.classList.remove('primary-image')
+  console.log('Translate some shit')
+  lastImageIndex = index
+}
+
+const setPositionTrackerActiveState = (trackers, activeTrackerIndex) => {
+  trackers.forEach(tracker => {
+    tracker.classList.remove('in-focus')
+  })
+  trackers[activeTrackerIndex].classList.add('in-focus')
+}
+
+positionTrackers.forEach((tracker, index) => {
+  tracker.addEventListener('click', () => navigateCarousel(index))
+})
+
+carousel.forEach((el, index) => {
+  el.addEventListener('click', () => navigateCarousel(index))
+})
+
