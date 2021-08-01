10a6111613973fe4fb503d903debfb48a9a09580
Tenotea <devtenotea@gmail.com>
2021-08-01 03:26:03 +0100

    update(hero): added gesture controls to hero carousel

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,4 @@
+const heroSection = document.querySelector('.hero-section')
 const positionTrackers = document.querySelectorAll('.position-trackers .tracker')
 const carousel = document.querySelectorAll('.secondary-image')
 const carouselBodies = document.querySelectorAll('.carousel-body')
@@ -11,7 +12,6 @@ const navigateCarousel = (index) => {
   let lastImage = carousel[lastImageIndex]
   currentImage.classList.add('primary-image')
   lastImage.classList.remove('primary-image')
-  console.log('Translate some shit')
   lastImageIndex = index
 }
 
@@ -29,6 +29,44 @@ const setCarouselBodyActiveState = (bodies, activeIndex) => {
   bodies[activeIndex].classList.add('in-focus')
 }
 
+let dragStartPosition = 0
+let dragEndPosition = 0
+
+const handleMousePress = (e) => {
+  if (e.changedTouches && e.changedTouches[0]) {
+    dragStartPosition = e.changedTouches[0].clientX
+  } else {
+    dragStartPosition = e.clientX
+  }
+}
+
+const mouseDragGesture = (e) => {
+  if (e.changedTouches && e.changedTouches[0]) {
+    dragEndPosition = e.changedTouches[0].clientX
+  } else {
+    dragEndPosition = e.clientX
+  }
+    // dragEndPosition = e.clientX
+    if(dragEndPosition - dragStartPosition > 30){
+      if (lastImageIndex > 0) {
+        navigateCarousel(lastImageIndex - 1)
+      }
+    }
+    if(dragEndPosition - dragStartPosition < -30){
+      if (lastImageIndex < positionTrackers.length - 1) {
+        navigateCarousel(lastImageIndex + 1)
+      }
+    }
+}
+
+const handleTouchPress = (e) => {
+
+}
+
+const touchSwipeGesture = (e) => {
+
+}
+
 positionTrackers.forEach((tracker, index) => {
   tracker.addEventListener('click', () => navigateCarousel(index))
 })
@@ -37,4 +75,9 @@ carousel.forEach((el, index) => {
   el.addEventListener('click', () => navigateCarousel(index))
 })
 
+heroSection.addEventListener('mousedown', handleMousePress)
+heroSection.addEventListener('touchstart', handleMousePress)
+
+heroSection.addEventListener('mouseup', mouseDragGesture)
+heroSection.addEventListener('touchend', mouseDragGesture)
 
